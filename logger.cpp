fd5167e3e3d2facc5975147e69a3e1909dc1e2c1
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-09-12 10:49:33 +0900

    Add files via upload
    例

---
+++ logger.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Log.h"
+
+
+int main()
+{
+
+    const char * filename = "test.txt";
+    const char * mode = "a";
+
+    Logger logger;
+
+    logger.debug(filename, mode, (const char*)"debug\n");
+    logger.info(filename, mode, (const char*)"info\n");
+    logger.warn(filename, mode, (const char*)"warning!\n");
+
+}
